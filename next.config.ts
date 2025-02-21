088784d3bd367848abe6894dac9c535fa1d7dbb8
Nugo Bibilashvili <bibilashvili6@gmail.com>
2025-02-21 12:13:02 -0800

    next jonfig

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  exportTrailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
